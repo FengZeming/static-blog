33ef0533bc50fe0015bc0e19f0ecefd032ce8d17
sirui.yu <ysr_wymail@163.com>
2019-08-13 17:05:48 +0800

    fix regExp error

--- src/util.js
+++ src/util.js
@@ -1,7 +1,6 @@
 export function processMarkdownImgEnv(content) {
   if (process.env.NODE_ENV === 'production') {
-    const search = new RegExp('](/static/img/', 'g');
-    return content.replace(search, '](/static-blog/static/img/')
+    return content.replace(/\]\(\/static\/img/g, '](/static-blog/static/img')
   }
   return content
 }
