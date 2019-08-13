1b62b428fe8ddb0dff0a459532b8251334ab03c7
sirui.yu <ysr_wymail@163.com>
2019-08-13 16:52:42 +0800

    fix image baseURL

--- src/util.js
+++ src/util.js
@@ -1,6 +1,7 @@
 export function processMarkdownImgEnv(content) {
   if (process.env.NODE_ENV === 'production') {
-    return content.replace('](/static/img/', '](/static-blog/static/img/')
+    const search = new RegExp('](/static/img/', 'g');
+    return content.replace(search, '](/static-blog/static/img/')
   }
   return content
 }
